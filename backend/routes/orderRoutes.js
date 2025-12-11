17f446b64d0a0ebd8444bc7dffde16ef74d23271
bayu-prnd <bayu.prananda_ti24@nusaputra.ac.id>
2025-12-11 16:56:56 +0700

    Delete backend directory

--- backend/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const orderCtrl = require("../controllers/orderController");
-
-router.get("/", orderCtrl.getOrders);
-router.post("/", orderCtrl.createOrder);
-router.put("/:id/confirm", orderCtrl.confirmOrder);
-
-module.exports = router;
